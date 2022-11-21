95d178830e822aaa6b645662feafb370f3400cc4
PhantomSurfer <106287358+PhantomSurfer@users.noreply.github.com>
2022-11-21 19:15:53 +0800

    Update new_item.php

--- Final-Project/new_item.php
+++ Final-Project/new_item.php
@@ -28,18 +28,21 @@ if (!empty($_POST["newProductName"])) {
 
     // Check if file already exists
     if (file_exists($target_file)) {
+    echo '<script>alert("Sorry, Image File already exists.")</script>';
     echo "Sorry, file already exists.";
     $uploadOk = 0;
     }
 
     // Check file size
     if ($_FILES["UploadImage"]["size"] > 500000) {
+    echo '<script>alert("Sorry, Image Files is too large.")</script>';
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
     }
 
     // Allow certain file formats
     if($imageFileType != "png") {
+    echo '<script>alert("Sorry, Only PNG files are allowed.")</script>';
     echo "Sorry, PNG files are allowed.";
     $uploadOk = 0;
     }
@@ -52,6 +55,7 @@ if (!empty($_POST["newProductName"])) {
     if (move_uploaded_file($_FILES["UploadImage"]["tmp_name"], $target_dir . $Product_Name . "." . $imageFileType)) {
         echo "The file ". htmlspecialchars( basename( $_FILES["UploadImage"]["name"])). " has been uploaded.";
     } else {
+	echo '<script>alert("An Error has Occurred! Updload Image File Failed")</script>';
         echo "Sorry, there was an error uploading your file.";
     }
     }
@@ -64,6 +68,7 @@ if (!empty($_POST["newProductName"])) {
     $check = "SELECT `Product_Name` FROM inventory WHERE `Product_Name` = '$Product_Name'";
     $check_run = mysqli_query($con, $check);
     if (mysqli_num_rows($check_run)==1){
+	echo '<script>alert("New Item Product already exists in stock.")</script>';
         header('Location: INV_Log.php');
     }
     else {
@@ -75,6 +80,7 @@ if (!empty($_POST["newProductName"])) {
             header('Location: INV_System.php');
     
         } else {
+	    echo '<script>alert("An Error has Occurred")</script>';
             echo "Error updating record: " . mysqli_error($con);
             header('Location: INV_System.php');
         }
